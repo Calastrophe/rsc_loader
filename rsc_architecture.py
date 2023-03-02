6ebebf23a4506b3c09d750b5ae71fff4723f7230
Calastrophe <74928681+Calastrophe@users.noreply.github.com>
2023-03-01 23:08:50 -0600

    refactor

--- rsc_architecture.py
+++ rsc_architecture.py
@@ -26,30 +26,17 @@ class RSCInstruction(Enum):
     ASHR = 14
     NOT = 15
 
-def disasm(data: bytes, size: int) -> Optional[Tuple[str, str]]:
-    match size:
-        case 8:
-            instr = struct.unpack('i', data[:4])[0]
-            match instr:
-                case 1 | 2 | 5 | 6:
-                    operand = struct.unpack('i', data[4:])[0]
-                    return (instr, operand)
-                case _:
-                    return (instr, " ")
-        case 4 | 5 | 6 | 7:
-            instr = struct.unpack('i', data[:4])[0]
-            match instr:
-                case 0 | 3 | 4 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15:
-                    return (instr, " ")
-                case _:
-                    return None
-        case _:
-            return None
-
-
-
-## TODO: REFACTOR THIS CODE, HOLY MOLY
-## NOTE: DO NOT USE THIS CODE FOR REFERENCE, IT IS HOT GARBAGE
+def disasm(data: bytes, size: int) -> Optional[Tuple[RSCInstruction, int, str]]:
+    if (size >= 4 and size <= 8):
+        instr = struct.unpack('i', data[:4])[0]
+        match instr:
+            case 1 | 2 | 5 | 6:
+                operand = struct.unpack('i', data[4:])[0]
+                return (RSCInstruction(instr).name, 8, operand)
+            case 0 | 3 | 4 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15:
+                return (RSCInstruction(instr).name, 4, "")
+    else:
+        return None
 
 class RSC(Architecture):
     name = 'rsc'
@@ -73,49 +60,32 @@ class RSC(Architecture):
     stack_pointer = 'sp'
 
     def get_instruction_info(self, data: bytes, addr: int) -> Optional[InstructionInfo]:
-        ## EVERYTHING IS 8 BYTES
-        ## CUT OFF 4 BYTES AND IDENTIFY THE LENGTH OF THE INSTRUCTION
-        ##
-        name, operand = struct.unpack('ii', data[:8])
+        values = disasm(data, len(data))
+        if values is not None:
+            name, length, operand = values
+        else:
+            return None
         res = InstructionInfo()
+        res.length = length
         match name:
-            case 1 | 2 | 5 | 6:
-                res.length = 8
-            case 0 | 3 | 4 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15:
-                res.length = 4
-            case _:
-                return None
-        match name:
-            case 5:
+            case "JMP":
                 assert(type(operand) == int)
                 res.add_branch(BranchType.UnconditionalBranch, operand)
-            case 6:
+            case "JMPZ":
                 assert(type(operand) == int)
                 res.add_branch(BranchType.TrueBranch, operand)
                 res.add_branch(BranchType.FalseBranch, addr+8)
-            case _:
-                pass
         return res
 
     def get_instruction_text(self, data: bytes, addr: int) -> Optional[Tuple[List['function.InstructionTextToken'], int]]:
-        if len(data) <= 8:
-            log_info(f"INSTR TEXT {str(data)} {str(len(data))}")
-        if disasm(data, len(data)):
-            data, operand = disasm(data, len(data))
+        values = disasm(data, len(data))
+        if values is not None:
+            name, length, operand = values
         else:
             return None
-        log_info(f"OUTPUT {data} {operand}")
-        match data:
-            case 1 | 2 | 5 | 6 :
-                name, operand = RSCInstruction(data).name, hex(operand)
-                ret_length = 8
-            case 0 | 3 | 4 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 :
-                name, operand = RSCInstruction(data).name, ""
-                ret_length = 4
-            case _:
-                return None
+        operand = hex(operand) if operand else ""
         tokens = [InstructionTextToken(InstructionTextTokenType.TextToken, name), InstructionTextToken(InstructionTextTokenType.TextToken, "  "), InstructionTextToken(InstructionTextTokenType.TextToken, operand)]
-        return tokens, ret_length
+        return tokens, length
 
     def get_instruction_low_level_il(self, data: bytes, addr: int, il: LowLevelILFunction) -> Optional[int]:
         return None
